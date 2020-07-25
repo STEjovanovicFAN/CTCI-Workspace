3ddbff7e15fa8989ad66cfdd970adec0efb81292
Stefan <jovanovicstefan69@gmail.com>
2020-07-25 15:50:37 -0600

    ch3 q2

---
+++ Ch3/Q2.java
@@ -0,0 +1,54 @@
+package Ch3;
+import java.util.*;
+
+public class Q2 {
+
+    public static class stackWithMin extends MyStack<Integer>{
+        Stack<Integer> min;
+
+        public stackWithMin(){
+            min = new Stack<Integer>();
+        }
+
+        public int min(){
+            if(min.isEmpty()){
+                return Integer.MAX_VALUE;
+            }
+
+            return min.peek();
+        }
+
+        public void push(int i){
+            if(i <= min()){
+                min.push(i);
+            }
+            super.push(i);
+        }
+
+        public Integer pop(){
+            int i = (int) super.pop();
+            if(i == min()){
+                min.pop();
+            }
+
+            return i;
+        }
+
+        
+    }
+
+    public static void main(String[] args) {
+        stackWithMin stack = new stackWithMin();
+        stack.push(3);
+        System.out.println(stack.min());
+        stack.push(1);
+        System.out.println(stack.min());
+        stack.push(7);
+        System.out.println(stack.min());
+        stack.pop();
+        stack.pop();
+        System.out.println(stack.min());
+    }
+
+    
+}
